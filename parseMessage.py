55b211cfd6a1c2ba5ea122b44f7253bac641b87a
andapra <alexanderdan.pratama@gmail.com>
2022-08-26 14:35:46 +0700

    update oop

--- parseMessage.py
+++ parseMessage.py
@@ -1,14 +1,14 @@
 import os, shutil
 
 class get_message():
-    def __init__(self, telegram_message):
-        self.telegram_message = telegram_message
+    def __init__(self) -> None:
+        pass
 
-    def parse_json(self):
-        chat_id = self.telegram_message['message']['chat']['id']
+    def parse_json(telegram_message):
+        chat_id = telegram_message['message']['chat']['id']
         try:
-            incoming_msg = self.telegram_message['message']['text']
-            person_reply = self.telegram_message['message']['from']['first_name']
+            incoming_msg = telegram_message['message']['text']
+            person_reply = telegram_message['message']['from']['first_name']
         except:
             incoming_msg = 'There is no message'
             person_reply = 'There is no person/usernmae who reply'
